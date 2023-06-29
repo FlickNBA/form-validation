c057de775963795065b49b92bcd2d2c7c75a6e0d
FlickNBA <flicknba@icloud.com>
2023-06-29 15:04:45 +0200

    AAA

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./reset.css";
+import "./index.css";
+
+// const form = document.querySelector("form");
+
+// const mail = document.getElementById("mail");
+
+// const country = document.getElementById("country");
+
+// const zip = document.getElementById("zip");
+
+// const passOne = document.getElementById("passOne");
+
+// const passTwo = document.getElementById("passTwo");
+
+// mail.addEventListener("input", (event) => {
+//   if (mail.validity.typeMismatch) {
+//     mail.setCustomValidity("Put in correct email address!");
+//     showError("#mail + span.error", "abc");
+//   } else {
+//     mail.setCustomValidity("");
+//   }
+// });
+
+// const showError = (selector, message) => {
+//   let element = document.querySelector(selector);
+//   element.textContent = message;
+//   element.className = "error active";
+// };
